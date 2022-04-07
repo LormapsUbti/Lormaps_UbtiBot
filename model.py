1132973200e34308aae58a891b0b9998797a313a
LormapsUbti <Kusokdolbaeba1286@gmail.com>
2022-04-07 21:45:03 +0300

    add Horoscope module

--- model.py
+++ model.py
@@ -2,20 +2,17 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 
 def get_model_answer(zodiac_sign):
-    # tok = GPT2Tokenizer.from_pretrained("/VVEDI/SVOI/PUT")
-    # model = GPT2LMHeadModel.from_pretrained("/VVEDI/SVOI/PUT")
-    # model.cuda()
-    #
-    # text = 'Гороскоп: '
-    # input = tok.encode(text, return_tensors="pt")
-    #
-    # out = model.generate(input.cuda(), max_length=100, repetition_penalty=5.0, do_sample=True, top_k=5, top_p=0.95,
-    #                      temperature=0.7)
-    # return tok.decode(out[0])
+    tok = GPT2Tokenizer.from_pretrained("/VVEDI/SVOI/PUT")
+    model = GPT2LMHeadModel.from_pretrained("/VVEDI/SVOI/PUT")
+    model.cuda()
+
+    text = 'Гороскоп: '
+    input = tok.encode(text, return_tensors="pt")
+
+    out = model.generate(input.cuda(), max_length=100, repetition_penalty=5.0, do_sample=True, top_k=5, top_p=0.95,
+                         temperature=0.7)
 
     answer = f'Гороскоп на сегодня для {zodiac_sign}:'
-    # predict_horo = answer + tok.decode(out[0]).split()[0]
-    # return predict_horo
+    predict_horo = answer + str(tok.decode(out[0]).split()[0])
+    return predict_horo
 
-    # Удали меня и ниже
-    return answer
